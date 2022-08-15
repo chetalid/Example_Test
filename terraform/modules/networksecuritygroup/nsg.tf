6c6ce7cbc430818935ec2930000094fdbbecd47b
chetali <dchaitali11@gmail.com>
2022-08-15 15:44:17 +0530

    changes

--- terraform/modules/networksecuritygroup/nsg.tf
+++ terraform/modules/networksecuritygroup/nsg.tf
@@ -1,5 +1,5 @@
 resource "azurerm_network_security_group" "nsg" {
-  name                = "${var.application_type}-${var.resource_type}"
+  name                = "web-${var.resource_type}"
   location            = "${var.location}"
   resource_group_name = "${var.resource_group}"
 
--- terraform/modules/publicip/input.tf
+++ terraform/modules/publicip/input.tf
@@ -2,5 +2,9 @@
 variable "location" {}
 variable "resource_group" {}
 
-variable "application_type" {}
-variable "resource_type" {}
+variable "application_type" {
+    default = "web"
+}
+variable "resource_type" {
+    default = "pubIP"
+}
