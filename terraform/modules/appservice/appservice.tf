aed7d77be36cac69a4d88163aef3fe20069fb08d
chetali <dchaitali11@gmail.com>
2022-08-15 14:17:27 +0530

    app service fix

--- terraform/modules/appservice/appservice.tf
+++ terraform/modules/appservice/appservice.tf
@@ -1,12 +1,10 @@
-resource "azurerm_app_service_plan" "test" {
+resource "azurerm_service_plan" "test" {
   name                = "${var.application_type}-${var.resource_type}"
   location            = var.location
   resource_group_name = var.resource_group
+  sku_name            = "F1"
+  os_type             = "Linux"
   
-  sku {
-    tier = "Free"
-    size = "F1"
-  }
 }
 
 resource "azurerm_app_service" "test" {
